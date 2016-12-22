1bee8b3376ebcb8005b9b7e04823200276acc71b
Tatiana Limache Quispe <tati34che@gmail.com>
2016-12-22 08:11:32 -0500

    Ejercicio Terminado sin estilos

---
+++ README.md
@@ -0,0 +1,26 @@
+#Ejercicio Parrot Trouble
+
+*Inicio: Parrot Trouble*
+
+## Ingreso datos
+- talking
+- hora
+
+
+## Proceso
+
+Obtener valor talking
+Obtener valor hora
+
+Si talking es verdadero
+	Si hora es menor a 7 ó mayor a 20
+		mostrar 1
+	si no
+		mostrar 0
+
+Si talking es falso
+	Si hora es menor ó igual a 23
+		mostrar 0
+Fin si
+
+*Fin*
---
+++ main.js
@@ -0,0 +1,35 @@
+
+function calcular()
+{
+	var numero1 = document.getElementById("numero1").value;
+	var numero2 = parseInt(document.getElementById("numero2").value);
+	var salida = document.getElementById("salida");
+	var negative;
+
+	if (numero1 < 0 && numero2 < 0 ) 
+	{
+		negative=true;
+		if (negative) 
+			{
+				salida.innerHTML = "1";
+				document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger">Numeros negativos</div>';
+			}
+		
+	}
+	else if (numero1 > 0 && numero2 > 0 ) 
+	{
+		negative=false;
+		if (!negative) 
+			{
+				document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger">Numeros Positivos</div>';
+				salida.innerHTML = "0";
+			}
+	}
+	else 
+	{
+		salida.innerHTML = "1";
+		document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger">Uno positivo otro negativo</div>';
+	}
+
+
+}
